1b3d68fabb8018f85f2e8b3eff3551ed1b3ae7cd
Norbert Grover <norbert298@gmail.com>
2024-08-19 16:41:04 -0700

    Initial Commit

---
+++ CombineAPICallVIPER/CombineAPICallVIPERApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineAPICallVIPERApp.swift
+//  CombineAPICallVIPER
+//
+//  Created by Norbert Grover on 8/19/24.
+//
+
+import SwiftUI
+
+@main
+struct CombineAPICallVIPERApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
